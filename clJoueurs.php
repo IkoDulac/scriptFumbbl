018bf7b83b1cfe77a2906da1ff6c9a1ecc4739c4
iko <31493278+IkoDulac@users.noreply.github.com>
2018-05-08 11:57:43 -0400

    Update clJoueurs.php

--- clJoueurs.php
+++ clJoueurs.php
@@ -1,45 +1,57 @@
 #!/usr/bin/php
 <?php
-$rawData = file_get_contents('https://fumbbl.com/xml:group?id=9761&op=matches');
-$rawXML = simplexml_load_string($rawData);
 
-//Stock les variables des joueurs
+$rawData = file_get_contents('testfile.xml') or die("error: cannot fetch url");
+$rawXML = simplexml_load_string($rawData) or die("error: cannot create object");
+
 $players=array();
 
-//Building the stats
-foreach($rawXML->matches as $row){
-    foreach($row->match as $match){
-        foreach($match->home->performances->performance as $performance){
-            //Ici on a les joueurs isolé
-            $perf = current($performance);
+function pdata() {
+        
+    global $players, $perf;
 
-            //Nouveau joueur
-            if(empty($players[$perf['player']])){
-                $players[$perf['player']] = $perf;
-            }else{
-                //Joueur existant on add les stats
-                $data = $players[$perf['player']];
+    if(empty($players[$perf['player']])){
+        $players[$perf['player']] = $perf;
+    }else{ 
 
-                $data['completions'] = $data['completions'] + $perf['completions'];
-                $data['touchdowns'] = $data['touchdowns'] + $perf['touchdowns'];
-                $data['interceptions'] = $data['interceptions'] + $perf['interceptions'];
-                $data['casualties'] = $data['casualties'] + $perf['casualties'];
-                $data['mvps'] = $data['mvps'] + $perf['mvps'];
-                $data['passing'] = $data['passing'] + $perf['passing'];
-                $data['rushing'] = $data['rushing'] + $perf['rushing'];
-                $data['blocks'] = $data['blocks'] + $perf['blocks'];
-                $data['fouls'] = $data['fouls'] + $perf['fouls'];
+    $data = $players[$perf['player']];
 
-                $players[$perf['player']] = $data;
-                unset($data);
-            }
-        }
+    $data['completions'] += $perf['completions'];
+    $data['touchdowns'] += $perf['touchdowns'];
+    $data['interceptions'] += $perf['interceptions'];
+    $data['casualties'] += $perf['casualties'];
+    $data['mvps'] += $perf['mvps'];
+    $data['passing'] += $perf['passing'];
+    $data['rushing'] += $perf['rushing'];
+    $data['blocks'] += $perf['blocks'];
+    $data['fouls'] += $perf['fouls'];
+    $data['turns'] += $perf['turns'];
+
+    $players[$perf['player']] = $data;
     }
+    return $players[$perf['player']];
 }
 
-$fp = fopen('players.csv', 'w');
+foreach($rawXML->matches as $matches) {
+    foreach($matches->match as $match) {
+        foreach($match->home->performances->performance as $performance) {
 
-//Building the header
+        $perf = current($performance);
+        $players[$perf['player']] = pdata();    
+        }
+
+        foreach($match->away->performances->performance as $performance) {
+
+        $perf = current($performance);
+        $players[$perf['player']] = pdata();    
+        }
+    }  
+}
+
+
+$fp = fopen('playerstest.csv', 'w');
+
+//entete
 $headers = current($players);
 $val = array();
 foreach($headers as $key => $header){
@@ -49,15 +61,17 @@ foreach($headers as $key => $header){
 fputcsv($fp, $val);
 
 
+
 echo '[table]';
 foreach ($players as $player) {
     echo '[tr]';
     fputcsv($fp, $player);
     foreach($player as $pdata){
-        echo '[td]'.$pdata.'[/td]';
+       echo '[td]'.$pdata.'[/td]';
     }
     echo '[/tr]';
 }
 echo '[/table]';
 fclose($fp);
+
 ?>
